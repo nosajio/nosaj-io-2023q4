22923ef6308adebbf32c48041570ba3d7f19aa30
Jason Howmans <jason@nosaj.io>
2023-10-23 23:48:22 +0100

    Prevent multiple client prisma error

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,4 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient();
+const isDev = process.env.NODE_ENV !== 'production';
+
+const prismaSingleton = () => {
+  return new PrismaClient(isDev ? { log: ['query'] } : undefined);
+};
+type PrismaClientSingleton = ReturnType<typeof prismaSingleton>;
+
+const globalThisWithPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton;
+};
+
+if (!globalThisWithPrisma.prisma) {
+  globalThisWithPrisma.prisma = prismaSingleton();
+}
+
+const client = globalThisWithPrisma.prisma ?? prismaSingleton();
+
 export default client;
+
+if (isDev) {
+  globalThisWithPrisma.prisma = client;
+}
