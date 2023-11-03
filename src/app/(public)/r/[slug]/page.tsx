d756e508b2881b3d522f2cd77f17315522558cb1
Jason Howmans <jason@nosaj.io>
2023-11-03 17:30:50 +0000

    Metadata & OG

--- src/app/(public)/r/[slug]/page.tsx
+++ src/app/(public)/r/[slug]/page.tsx
@@ -1,19 +1,41 @@
 import Post from '@/components/post/Post';
 import client from '@/db/client';
+import { Metadata } from 'next';
 import Head from 'next/head';
-
-export async function generateStaticParams() {
-  const posts = await client.post.findMany({
-    where: { published: true },
-  });
-  return posts.map(p => ({
-    slug: p.slug,
-  }));
-}
 
 type PostPageProps = {
   params: {
     slug: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params: { slug },
+}: PostPageProps): Promise<Metadata> => {
+  const post = await client.post.findUnique({
+    where: { slug, published: true },
+  });
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.blurb || '',
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
+    openGraph: {
+      images: post?.coverImage ? [post.coverImage] : undefined,
+      title: post.title,
+      description: post.blurb || '',
+      type: 'article',
+    },
+    twitter: {
+      title: post.title,
+      description: post.blurb || '',
+      images: post?.coverImage ? [post.coverImage] : undefined,
+      card: 'summary_large_image',
+      creator: '@nosajio',
+    },
   };
 };
 
